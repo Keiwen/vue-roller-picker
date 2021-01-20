2c7be46b6934d19f236cb59c24ae6488213fd589
Keiwen <keiwen.mk@gmail.com>
2021-01-20 09:08:01 +0100

    switch sample list

--- docs/views/Main.vue
+++ docs/views/Main.vue
@@ -7,7 +7,7 @@
             <div class="playWith">
                 <h2>Play with it</h2>
 
-                <roller-picker :options="options_list"
+                <roller-picker :options="rp_option_list"
                            :line-height-px="parseInt(rp_line_height)"
                            :font-size="rp_fontsize"
                            :disabled="rp_disabled"
@@ -55,6 +55,15 @@
                 <label for="rp_infinite">Infinite scroll:</label>
                 <input type="checkbox" v-model="rp_infinite" id="rp_infinite"/>
 
+                <br/>
+                <hr/>
+
+                <label for="rp_list">Sample list:</label>
+                <select v-model="rp_list" id="rp_list">
+                    <option value="long">Long</option>
+                    <option value="short">Short</option>
+                </select>
+
             </div>
 
             <div class="gallery">
@@ -77,7 +86,7 @@
     data () {
       return {
         options_list: ['Apple', 'Apricot', 'Banana', 'Blueberry', 'Cherry', 'Grape', 'Kiwi', 'Lemon', 'Orange', 'Peach', 'Pear', 'Pineapple', 'Raspberry', 'Strawberry'],
-        options_short: ['Apple', 'Kiwi', 'Strawberry'],
+        options_short: ['Cat', 'Dog', 'Mouse'],
         options_objects: [{'name': 'Apple', 'formValue': 'ap'}, {'name': 'Kiwi', 'formValue': 'ki'}, {'name': 'Peach', 'formValue': 'pe'}],
         rp_value: 'Kiwi',
         rp_line_height: '40',
@@ -86,7 +95,17 @@
         rp_big: false,
         rp_jump: false,
         rp_infinite: false,
-        rp_animated: true
+        rp_animated: true,
+        rp_list: 'long'
+      }
+    },
+    computed: {
+      rp_option_list () {
+        switch (this.rp_list) {
+          case 'long': return this.options_list
+          case 'short': return this.options_short
+          default: return []
+        }
       }
     }
   }
